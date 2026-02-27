dfde6860ee05f2043e5f500090847b836df47616
priyanshurai262-dev <priyanshurai262@gmail.com>
2026-02-27 13:44:44 +0530

    Create Tailwind CSS configuration file with black and red theme colors

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        black: '#000000',
+        red: '#FF0000',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
